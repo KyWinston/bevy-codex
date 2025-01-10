79471e812f1c853c2258a77caba2d00e4d1473db
KyleWinston <kyle.w.r@hotmail.com>
2025-01-10 00:48:28 -0500

    input changes

--- src/widgets/input/systems.rs
+++ src/widgets/input/systems.rs
@@ -1,9 +1,23 @@
 use bevy::prelude::*;
-use bevy_hui::prelude::HtmlComponents;
+use bevy_hui::prelude::{HtmlComponents, HtmlFunctions};
+use bevy_hui_widgets::prelude::TextInput;
 
-pub fn register_input(mut comps: HtmlComponents, assets: Res<AssetServer>) {
+pub fn register_input(
+    mut comps: HtmlComponents,
+    assets: Res<AssetServer>,
+    mut funcs: HtmlFunctions,
+) {
     comps.register(
         "input",
         assets.load("embedded://bevy_codex/widgets/input/input.html"),
+    );
+    funcs.register(
+        "notify_input_change",
+        |In(entity), inputs: Query<&TextInput>| {
+            let Ok(input) = inputs.get(entity) else {
+                return;
+            };
+            info!("Input {entity} changed, new value: `{}`", input.value);
+        },
     );
 }
